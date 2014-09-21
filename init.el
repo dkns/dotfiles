1513f8fedc607006c2f5ead58e11729935537b5b
Daniel Kosinski <de.kosinski@gmail.com>
2014-09-22 00:30:07 +0200

    Fix smooth scrolling

--- init.el
+++ init.el
@@ -205,4 +205,7 @@ re-downloaded in order to locate PACKAGE."
    '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
 
 ;; smooth scrolling
-(require smooth-scrolling)
+(require 'smooth-scrolling)
+(setq smooth-scroll-margin 5)
+(setq scroll-conservatively 9999
+      scroll-preserve-screen-position t)
