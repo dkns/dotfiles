b3b3900677668634f88df1085e30844fae3d749e
Daniel Kosinski <de.kosinski@gmail.com>
2014-09-22 23:04:18 +0200

    Add relative line numbers

--- init.el
+++ init.el
@@ -245,3 +245,30 @@ re-downloaded in order to locate PACKAGE."
 
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized))))) ;; start maximized
+
+(defvar my-linum-format-string "%5d")
+
+(add-hook 'linum-before-numbering-hook 'my-linum-get-format-string)
+
+(defun my-linum-get-format-string ()
+  ; The + 2 defines the leeway we have in the sidebar
+  (let* ((width (+ 3 (length (number-to-string
+                             (count-lines (point-min) (point-max))))))
+         (format (concat "%" (number-to-string width) "d")))
+    (setq my-linum-format-string format)))
+
+(defvar my-linum-current-line-number 0)
+
+(setq linum-format 'my-linum-relative-line-numbers)
+
+;(propertize (format my-linum-format-string noffset) 'face 'linum)
+(defun my-linum-relative-line-numbers (line-number)
+    (let ((offset (- line-number my-linum-current-line-number)))
+      (if (= offset 0)
+          (propertize (format my-linum-format-string line-number) 'face 'linum)
+          (propertize (format my-linum-format-string (abs offset)) 'face 'linum))))
+
+(defadvice linum-update (around my-linum-update)
+             (let ((my-linum-current-line-number (line-number-at-pos)))
+                   ad-do-it))
+(ad-activate 'linum-update)
