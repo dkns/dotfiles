69b14030fd0e4e9161ac34bd175bd2a8bc28c462
Daniel Kosinski <de.kosinski@gmail.com>
2014-11-14 12:38:20 +0100

    Emacs: install jedi, change how autosave works

--- init.el
+++ init.el
@@ -48,6 +48,7 @@ re-downloaded in order to locate PACKAGE."
    projectile
    dash
    smart-mode-line
+   jedi
    ))
 
 ;; Package manager and packages handler
@@ -170,14 +171,6 @@ re-downloaded in order to locate PACKAGE."
 ;; highlighted
 
 (global-hl-line-mode 1)
-
-;; ========== Place Backup Files in Specific Directory ==========
-
-;; Disable backup files.
-(setq make-backup-files nil)
-
-;; Enable versioning with default values (keep five last versions, I think!)
-(setq version-control t)
 
 ;; ========== Enable Line and Column Numbering ==========
 
@@ -312,3 +305,25 @@ re-downloaded in order to locate PACKAGE."
 ;; smart mode line
 (require 'smart-mode-line)
 (sml/setup)
+
+(defvar backup-directory "~/.backups")
+(if (not (file-exists-p backup-directory))
+    (make-directory backup-directory t))
+(setq
+ make-backup-files t        ; backup a file the first time it is saved
+ backup-directory-alist `((".*" . ,backup-directory)) ; save backup files in ~/.backups
+ backup-by-copying t     ; copy the current file into backup directory
+ version-control t   ; version numbers for backup files
+ delete-old-versions t   ; delete unnecessary versions
+ kept-old-versions 6     ; oldest versions to keep when a new numbered backup is made (default: 2)
+ kept-new-versions 9 ; newest versions to keep when a new numbered backup is made (default: 2)
+ auto-save-default t ; auto-save every buffer that visits a file
+ auto-save-timeout 20 ; number of seconds idle time before auto-save (default: 30)
+ auto-save-interval 200 ; number of keystrokes between auto-saves (default: 300)
+ )
+
+;; set font
+(set-default-font "Inconsolata-11")
+
+;; autocompletion for python
+(add-hook 'python-mode-hook 'jedi:setup)
