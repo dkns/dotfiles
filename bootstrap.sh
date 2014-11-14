aafd244ae87520a3b5b8d38e9780e5b9dfad9d55
Daniel Kosinski <de.kosinski@gmail.com>
2014-11-14 17:09:37 +0100

    Bootstrap: add more todos

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,13 @@
 
 # TODO: install i3, bin scripts, emacs.d, awesome?, Xdefaults
 # required programs: urxvt, wmctrl (used for maximizing gvim), vim-gnome, emacs,
-# inconsolata font, some stuff for installing youcompleteme
+# inconsolata font, some stuff for installing youcompleteme, ctags, virtualenv, virtualenvwrapper
+# ranger, cmus, i3blocks, curl, LAMP stack, dropbox, libre office suite, ack-grep, fzf, zsh, oh-my-zsh
+
+# TODO: consider these:
+# hosts file?
+#
+
+# TODO: remove not needed programs?
+#
+echo "All done. Welcome on board, Commander."
