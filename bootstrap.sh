1167cfeb5f0b16dee34ddc011ca63bd1162b0114
Daniel Kosinski <de.kosinski@gmail.com>
2014-11-14 19:35:09 +0100

    Bootstrap: main functionality

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -exv
 # TODO: make this go through every file/dir in dotfiles repo instead of manually typing which
 # files/dirs should be linked
 
@@ -13,4 +13,45 @@
 
 # TODO: remove not needed programs?
 #
-echo "All done. Welcome on board, Commander."
+
+check_programs=(vim-gnome i3 curl cmus ranger zsh \
+exuberant-ctags fonts-inconsolata rxvt-unicode-265color \
+ack-grep)
+
+install_programs=()
+
+if [ $(whoami) != 'root' ]; then
+    echo "Insufficient privileges found. Must be root."
+    echo "Exiting."
+    exit 1;
+fi
+
+echo "Updating sources list..."
+#apt-get update
+echo "Checking previous installations..."
+for program in ${check_programs[@]}; do
+    echo "Checking $program"
+    if dpkg -s $program >/dev/null 2>&1; then
+        echo "found $program"
+    else
+        echo "$program not found."
+        echo "Searching for $program in repositories."
+        search="$(apt-cache search $program)"
+        if [ -n "$search" ]; then
+            echo "Adding $program to install list."
+            install_programs+=($program)
+        else
+            echo "Couldn't find $program, skipping."
+        fi
+        echo "Checking complete."
+    fi
+done
+
+echo "Installing required programs..."
+for program in ${install_programs[@]}; do
+    echo "Installing $program"
+    apt-get install $program
+done
+
+echo "All done."
+echo "Welcome on board, Commander."
