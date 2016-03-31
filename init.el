0e8db164440d907e20c8e07668af3ba9578fe558
Daniel Kosinski <de.kosinski@gmail.com>
2016-04-01 00:25:38 +0200

    Emacs: basic stuff

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;;; package --- Summary
+;;; Code:
+;;; Commentary:
+
+(setq load-prefer-newer t)
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
@@ -10,33 +16,108 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; basic config stuff
+(setq-default indent-tabs-mode nil) ;; don't use spaces
+(global-auto-revert-mode t) ;; automatically refresh buffers when file changes
+
+(eval-when-compile
+  (require 'use-package))
+
+(require 'bind-key)
+(require 'diminish)
+
 ;; open splits
-(defun my-open-window-vertically ()
+(defun dkns/open-window-vertically ()
   (interactive)
   (split-window-vertically)
   (windmove-down))
-(defun my-open-window-horizontally ()
+(defun dkns/open-window-horizontally ()
   (interactive)
   (split-window-horizontally)
   (windmove-right))
 
-(use-package evil-leader
-  :commands (evil-leader-mode global-evil-leader-mode)
-  :ensure evil-leader
-  :config
-  (progn
-    (evil-leader/set-leader "<SPC>")
-    (global-evil-leader-mode t)))
+;; settings
+(show-paren-mode 1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(remove-hook 'find-file-hooks 'vc-find-file-hook)
 
+;; packages
 (use-package evil
   :ensure t
+  :init
+  (use-package evil-leader
+    :ensure
+    :config
+    (progn
+        (evil-leader/set-leader "<SPC>")
+        (global-evil-leader-mode t))
+    )
+  (with-eval-after-load 'evil
+    (defalias #'forward-evil-word #'forward-evil-symbol)) ;; treat _ and - as part of words
   :config
   (progn
     (evil-mode 1)
-    (setq evil-want-fine-undo t)))
+    (setq evil-want-fine-undo t))
+  )
 
 (use-package php-mode
   :ensure t
+  :defer t
   :config
   (progn
-    (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))))
+    (add-to-list 'auto-mode-alist '("\\.php$" . php-mode)))
+  )
+
+(use-package darktooth-theme
+  :ensure t
+  )
+
+(use-package js2-mode
+  :ensure t
+  :defer t
+  :config
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
+  )
+
+(use-package org
+  :ensure t
+  :defer t
+  )
+
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook 'flycheck-mode)
+  )
+
+(use-package which-func
+  :ensure t
+  :config
+  (which-function-mode)
+  )
+
+(use-package powerline
+  :ensure t
+  :config
+  (require 'powerline)
+  (powerline-vim-theme)
+  )
+
+(use-package kpm-list
+  :ensure t
+  :defer t
+  )
+
+(use-package which-key
+  :ensure t
+  :config
+  (require 'which-key)
+  (which-key-mode)
+  )
+
+(provide 'init)
+;;; init.el ends here
+
