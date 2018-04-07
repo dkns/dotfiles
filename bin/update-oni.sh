1a1d8dadc335dc58f5cc34a75aecb92816a9bd71
Daniel Kosinski <de.kosinski@gmail.com>
2018-04-07 23:30:04 +0200

    Bin: add script for updating oni

---
+++ bin/update-oni.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+set -euo pipefail
+
+release_url=$(curl -s -L https://github.com/onivim/oni/releases/latest | grep releases/download | grep amd64-linux | grep -o -P '"[a-zA-Z_\-.\/0-9]+"' | head -n 1 | tr -d '"')
+
+download_url="https://github.com$release_url"
+
+cd /tmp
+
+wget -O oni.deb $final_download_url
+
+sudo dpkg -i oni.deb
+
+rm oni.deb
