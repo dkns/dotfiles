cc2fbd666b31fe0e545c07910f1f78ab97970616
Daniel Kosinski <de.kosinski@gmail.com>
2018-05-24 00:05:21 +0200

    Vim: update oni config

--- oni/config.js
+++ oni/config.js
@@ -1,18 +1,44 @@
-const activate = (Oni) => {
-   console.log("config activated")
-}
+"use strict";
+exports.__esModule = true;
+exports.activate = function (oni) {
+    console.log("config activated");
+    // Input
+    //
+    // Add input bindings here:
+    //
+    oni.input.bind("<c-enter>", function () { return console.log("Control+Enter was pressed"); });
+    //
+    // Or remove the default bindings here by uncommenting the below line:
+    //
+    //oni.input.unbind("<c-p>");
 
-const deactivate = () => {
-   console.log("config deactivated")
-}
-
-module.exports = {
-   activate,
-   deactivate,
-  "oni.useDefaultConfig": true,
-  //"oni.bookmarks": ["~/Documents",]
-  "oni.loadInitVim": true,
-  //"editor.fontSize": "14px",
-  //"editor.fontFamily": "Monaco"
-  "editor.textMateHighlighting.enabled": true
-}
+    oni.input.bind("<c-h>", () =>
+        oni.editors.activeEditor.neovim.command(`call OniNextWindow('h')<CR>`)
+    )
+    oni.input.bind("<c-j>", () =>
+        oni.editors.activeEditor.neovim.command(`call OniNextWindow('j')<CR>`)
+    )
+    oni.input.bind("<c-k>", () =>
+        oni.editors.activeEditor.neovim.command(`call OniNextWindow('k')<CR>`)
+    )
+    oni.input.bind("<c-l>", () =>
+        oni.editors.activeEditor.neovim.command(`call OniNextWindow('l')<CR>`)
+    )
+};
+exports.deactivate = function (oni) {
+    console.log("config deactivated");
+};
+exports.configuration = {
+    //add custom config here, such as
+    "ui.colorscheme": "onedark",
+    //"oni.useDefaultConfig": true,
+    //"oni.bookmarks": ["~/Documents"],
+    "oni.loadInitVim": true,
+    "editor.fontSize": "12px",
+    //"editor.fontFamily": "Monaco",
+    // UI customizations
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto",
+    "experimental.preview.enabled": true,
+    "tabs.mode": "tabs"
+};
