3565f1026a2d4d48c659ec5d36028af138302c1b
Daniel Kosiński <de.kosinski@gmail.com>
2021-05-03 18:59:00 +0200

    Nvim: add utils

---
+++ nvim/lua/dkns/utils.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+M.get_os = function()
+  local system_name
+  if vim.fn.has('mac') == 1 then
+    system_name = 'macOS'
+  elseif vim.fn.has('unix') == 1 then
+    system_name = 'Linux'
+  elseif vim.fn.has('win32') == 1 then
+    system_name = "Windows"
+  end
+
+  return system_name
+end
+
+return M
