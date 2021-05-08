a4fd92089a48435c8a1c530dae4cb3dea65cef84
Daniel Kosiński <de.kosinski@gmail.com>
2021-05-08 13:38:32 +0200

    Nvim: add keybind for editing init.lua

--- nvim/lua/dkns/keybinds.lua
+++ nvim/lua/dkns/keybinds.lua
@@ -11,5 +11,7 @@ map('n', '<leader>h', '<cmd>vsplit<cr>')
 map('n', '<leader>b', '<cmd>Telescope buffers<cr>')
 map('n', '<C-p>', '<cmd>Telescope find_files<cr>')
 
+map('n', '<leader>ev', ':e $HOME/dotfiles/nvim/init.lua<cr>')
+
 map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
 map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
