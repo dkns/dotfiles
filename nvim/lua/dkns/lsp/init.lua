2f4871ac3de406b19b0aac0f5a2135d2be0676c9
Daniel Kosiński <de.kosinski@gmail.com>
2022-03-12 14:33:33 +0100

    Vim: add lua lsp setup

--- nvim/lua/dkns/lsp/init.lua
+++ nvim/lua/dkns/lsp/init.lua
@@ -5,8 +5,30 @@ local function on_attach(client, bufnr)
   -- buffer local keymaps
 end
 
-local enhance_server_opts = {
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
 
+local enhance_server_opts = {
+  ['sumneko_lua'] = function(opts)
+    opts.settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT',
+          path = runtime_path
+        },
+        diagnostics = {
+          globals = { 'vim' }
+        },
+        workspace = {
+          library = vim.api.nvim_get_runtime_file("", true)
+        },
+        telemetry = {
+          enable = false
+        }
+      }
+    }
+  end,
 }
 
 lsp_installer.on_server_ready(function(server)
