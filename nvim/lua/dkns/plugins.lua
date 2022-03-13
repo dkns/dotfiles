9248be8615a9b1e894b0b32477df8b23fa3c2a60
Daniel Kosiński <de.kosinski@gmail.com>
2022-03-14 00:03:30 +0100

    Vim: add lualine

--- nvim/lua/dkns/plugins.lua
+++ nvim/lua/dkns/plugins.lua
@@ -1,5 +1,6 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local packer_bootstrap
 
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
@@ -25,6 +26,14 @@ return require('packer').startup(function(use)
       require('kanagawa').setup({
         dimInactive = true
       })
+    end
+  }
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = function()
+      require('lualine').setup({})
     end
   }
 
