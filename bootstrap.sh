a512c501e97de9931cf6947338d994aa8f19ed3d
Daniel Kosiński <de.kosinski@gmail.com>
2022-03-19 12:54:48 +0100

    Sh: update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,80 +1,37 @@
-#!/bin/bash -exv
-# TODO: make this go through every file/dir in dotfiles repo instead of manually typing which
-# files/dirs should be linked
+#!/bin/bash
+set -euo pipefail
 
-# TODO: install i3, bin scripts, emacs.d, awesome?, Xdefaults
-# required programs: urxvt, wmctrl (used for maximizing gvim), vim-gnome, emacs,
-# inconsolata font, some stuff for installing youcompleteme, ctags, virtualenv, virtualenvwrapper
-# ranger, cmus, i3blocks, curl, LAMP stack, dropbox, libre office suite, ack-grep, fzf, zsh, oh-my-zsh
-
-# need to make/compile other programs: libgnutls-dev
-
-# TODO: consider these:
-# hosts file?
-
-# TODO: remove not needed programs?
-
-base_programs=(vim-gnome i3 curl ranger zsh exuberant-ctags fonts-inconsolata\
-    rxvt-unicode ack-grep emacs python-pip firefox chromium-browser htop)
-work_programs=(subversion libreoffice)
-entertainment_programs=(cmus weechat)
-
-install_programs=()
-
-if [ $(whoami) != 'root' ]; then
-    echo "Insufficient privileges found. Must be root."
-    echo "Exiting."
-    exit 1;
-fi
-
-echo "Updating sources list..."
-#apt-get update
-
-# TODO: can this be abstracted away?
-echo "Would you like to install base programs?"
-if yes; then
-    echo 0
-fi
-echo "Would you like to install work programs?"
-if yes; then
-    echo 0
-fi
-echo "Would you like to install entertainment programs?"
-if yes; then
-    echo 0
-fi
-# TODO: put all of this stuff into seperate function
-
-function check_install {
-    echo "Checking previous installations..."
-    for program in ${check_programs[@]}; do
-        echo "Checking $program"
-        if dpkg -s $program >/dev/null 2>&1; then
-            echo "found $program"
-        else
-            echo "$program not found."
-            echo "Searching for $program in repositories."
-            search="$(apt-cache search $program)"
-            if [ -n "$search" ]; then
-                echo "Adding $program to install list."
-                install_programs+=($program)
-            else
-                echo "Couldn't find $program, skipping."
-            fi
-            echo "Checking complete."
-        fi
-    done
+function package_installed() {
+  dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -c "ok installed";
 }
 
-function install_programs {
-    echo "Installing required programs..."
-    for program in ${install_programs[@]}; do
-        echo "Installing $program"
-        apt-get install -y --force-yes $program
-    done
+function install_package() {
+  if [ $(package_installed "$1") -eq 0 ]; then
+    sudo apt-get install "$1"
+    echo "Installed $1"
+  else
+    echo "$1 already installed"
+  fi
 }
 
-#TODO: install automatically projects from git/github?
+function install_multiple_packages() {
+  sudo apt-get update;
 
-echo "All done."
-echo "Welcome on board, Commander."
+  for package in $1; do
+    install_package "$package";
+  done
+}
+
+function bootstrap_docker() {
+  install_multiple_packages "ca-certificates curl gnupg lsb-release"
+
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  install_multiple_packages "docker-ce docker-ce-cli containerd.io";
+}
+  
+bootstrap_docker
