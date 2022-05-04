0114fa67385fcb03d023027c4be90f059f395615
Daniel Kosiński <de.kosinski@gmail.com>
2022-05-04 22:17:55 +0200

    Vim: add vim-visual-multi

--- nvim/lua/dkns/plugins.lua
+++ nvim/lua/dkns/plugins.lua
@@ -120,6 +120,8 @@ return require('packer').startup(function(use)
     config = function() require('fidget').setup({}) end
   }
 
+  use 'mg979/vim-visual-multi'
+
   if packer_bootstrap then
     require('packer').sync()
   end
