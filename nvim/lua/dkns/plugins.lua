cf85f263cabcf641dd3fec5bedf229215e7bb9a6
Daniel Kosiński <de.kosinski@gmail.com>
2022-07-24 11:14:24 +0200

    Vim: remove old statusline plugin

--- nvim/lua/dkns/plugins.lua
+++ nvim/lua/dkns/plugins.lua
@@ -41,14 +41,6 @@ return require('packer').startup(function(use)
   }
 
   use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-    config = function()
-      require('lualine').setup({})
-    end
-  }
-
-  use {
     'lewis6991/gitsigns.nvim',
     requires = {
       'nvim-lua/plenary.nvim'
