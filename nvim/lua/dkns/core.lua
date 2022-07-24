d42ed1f77a798a9c9596d5d222a7f0a446420a7c
Daniel Kosiński <de.kosinski@gmail.com>
2022-07-24 11:13:17 +0200

    Vim: convert augroup to lua one

--- nvim/lua/dkns/core.lua
+++ nvim/lua/dkns/core.lua
@@ -1,6 +1,6 @@
-vim.cmd[[
-augroup highlight_yank
-autocmd!
-au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=550}
-augroup END
-]]
+local highlight_group = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  command = "silent! lua vim.highlight.on_yank { hlgroup='IncSearch', timeout=550 }",
+  group = highlight_group
+})
