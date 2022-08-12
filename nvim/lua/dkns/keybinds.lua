31206d0044028aaf7b909420c1774e90961ea576
Daniel Kosinski <de.kosinski@gmail.com>
2022-08-12 12:02:56 +0200

    Vim: recent files depend on OS

--- nvim/lua/dkns/keybinds.lua
+++ nvim/lua/dkns/keybinds.lua
@@ -1,4 +1,13 @@
 local map = require('dkns.utils').map
+local is_wsl = require('dkns.utils').is_wsl
+
+local recent_finder = function()
+  if is_wsl() then
+    return '<cmd>Telescope oldfiles<cr>'
+  end
+
+  return '<cmd>Telescope frecency<cr>'
+end
 
 map('n', '<leader>v', '<cmd>split<cr>')
 map('n', '<leader>h', '<cmd>vsplit<cr>')
@@ -8,8 +17,8 @@ map('n', '<leader>ev', ':e $HOME/dotfiles/nvim/init.lua<cr>')
 map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
 map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
 
-map('n', '<C-p>', '<cmd>Telescope find_files<cr>')
-map('n', '<leader>r', '<cmd>Telescope frecency<cr>')
+map('n', '<C-p>', '<cmd>Telescope find_files hidden=true<cr>')
+map('n', '<leader>r', recent_finder())
 map('n', '<leader>b', "<cmd>lua require('telescope.builtin').buffers{ show_all_buffers = true }<cr>")
 map('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>")
 map('n', '<leader>fp', ":lua require('telescope').extensions.project.project{}<cr>")
