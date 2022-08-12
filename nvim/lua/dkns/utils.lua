230bce0090e8bf97508369c94d87978e1f86ed6e
Daniel Kosinski <de.kosinski@gmail.com>
2022-08-12 12:02:22 +0200

    Vim: add helper functions for detecting OS

--- nvim/lua/dkns/utils.lua
+++ nvim/lua/dkns/utils.lua
@@ -1,10 +1,22 @@
 local M = {}
 
-
 function M.map(mode, lhs, rhs, opts)
   local options = {noremap = true}
   if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+function M.is_wsl()
+  local output = vim.fn.systemlist "uname -r"
+  return not not string.find(output[1] or "", "WSL")
+end
+
+function M.is_mac()
+  return vim.fn.has("maxunic") == 1
+end
+
+function M.is_linux(self)
+  return not self.is_wsl() and not self.is_mac()
 end
 
 return M
